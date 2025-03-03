5d95439fadca6f618372c95835f5dc6d1eb2c016
Srushti1811 <110477875+Srushti1811@users.noreply.github.com>
2025-03-03 12:39:06 +0530

    Add files via upload

---
+++ ifelseStatementsInPython.py
@@ -0,0 +1,26 @@
+#conditional Operators
+# < , > , >= , <= , == , !=
+
+a=int(input("Enter the value a : "))
+
+#if-else statement
+
+if(a>=50):
+    print("Your value is greater than 50")
+else:
+    print("Your value is less than 50 !!!")
+
+
+#if-else-elif statement
+
+num=int(input("Enter the value of num : "))
+
+if(num>0):
+    print("Value of num is positive : ")
+elif(num==0):
+    print("Value of num is Zero!!!")
+else:
+    print("Value of num is Negative..")
+
+#Nested if statement
+
