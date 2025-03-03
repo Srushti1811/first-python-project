078ebea353ab27471a350b12b9a20359c284bafe
Srushti <srushtibadachi@gmail.com>
2025-03-03 12:46:50 +0530

    changes made

---
+++ ifelseStatementsInPython.py
@@ -0,0 +1,35 @@
+#conditional Operators
+# < , > , >= , <= , == , !=
+
+# a=int(input("Enter the value a : "))
+
+# #if-else statement
+
+# if(a>=50):
+#     print("Your value is greater than 50")
+# else:
+#     print("Your value is less than 50 !!!")
+
+
+#if-else-elif statement
+
+# num=int(input("Enter the value of num : "))
+
+# if(num>0):
+#     print("Value of num is positive : ")
+# elif(num==0):
+#     print("Value of num is Zero!!!")
+# else:
+#     print("Value of num is Negative..")
+
+#Nested if statement
+
+b=int(input("Enter the number to check whther it is Even number or Odd number - "))
+if(b/2==0):
+    print("This is a Even number")
+elif(b/2!=0):
+    print("This is a Odd number")
+elif(b/2<0):
+    print("You have Entered a Negative number")
+else:
+    print("Enter a postive number")
